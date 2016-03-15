30db55155fbaf67a868188a642ba509c2ec98344
JustBecauseCodeIt <grigorichev@gmail.com>
2016-03-15 14:18:50 +0300

    from work

--- javarush/test/level06/lesson11/bonus03/Solution.java
+++ javarush/test/level06/lesson11/bonus03/Solution.java
@@ -27,13 +27,32 @@ public class Solution
     public static void main(String[] args) throws Exception
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> arr = new ArrayList<>();
+        int arr[] = new int[5];
         for(int i=0; i<5; i++)
         {
-            arr.add(Integer.parseInt(reader.readLine()));
+            arr[i] = Integer.parseInt(reader.readLine());
+
         }
-        Collections.sort(arr);
-        System.out.print(arr);
+     bubbleSort(arr);
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+
 
     }
+
+    public static void bubbleSort(int[] arr){
+           for(int i = arr.length-1 ; i > 0 ; i--)
+           {
+            for(int j = 0 ; j < i ; j++)
+            {
+                if( arr[j] > arr[j+1] )
+                {
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+                }
+             }
+            }
+}
 }
