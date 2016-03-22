8d4d1f1989816737d6b8cfc92167b9c274acd5dd
JustBecauseCodeIt <grigorichev@gmail.com>
2016-03-22 16:33:17 +0300

    from work

--- javarush/test/level07/lesson06/task05/Solution.java
+++ javarush/test/level07/lesson06/task05/Solution.java
@@ -14,7 +14,20 @@ public class Solution
 {
     public static void main(String[] args) throws Exception
     {
-        //напишите тут ваш код
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 5 ; i++) {
+            list.add(reader.readLine());
+        }
+        for (int i = 0; i < 13; i++) {
+            String s1 = list.remove(list.size() - 1);
+            list.add(0, s1);
+        }
+
+       for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+
+        }
 
     }
 }
